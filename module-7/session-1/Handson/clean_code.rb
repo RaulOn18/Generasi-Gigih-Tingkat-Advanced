1119ac9406fa911698806be38a9dbc1ab48b3b4f
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2021-08-16 18:11:34 +0700

    fix clean_code

--- module-7/session-1/Handson/clean_code.rb
+++ module-7/session-1/Handson/clean_code.rb
@@ -12,15 +12,15 @@ def register_user(params)
   user.save
   user.sendmailcnfrm
   user
-  end
+end
 
-  def invalid_user?
-    user.email.blank?\ 
-    ||user.username.blank? \
-    ||user.phone.blank? \
-    || !(user.email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i))||!(user.phone.match(/\A\+?[\d\s\-\.\(\)]+\z/))
-  end
+def invalid_user?
+  user.email.blank?\ 
+  ||user.username.blank? \
+  ||user.phone.blank? \
+  || !(user.email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i))||!(user.phone.match(/\A\+?[\d\s\-\.\(\)]+\z/))
+end
 
-  def duplicate_user?
-    Data.query("select count(1) from users where phone=#{a.phone} or email=#{a.email}") > 0
-  end
+def duplicate_user?
+  Data.query("select count(1) from users where phone=#{a.phone} or email=#{a.email}") > 0
+end
